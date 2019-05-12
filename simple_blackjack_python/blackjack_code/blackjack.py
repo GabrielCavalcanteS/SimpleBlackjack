6a50b888d0aef7b3b79a9840da32998f7c2794a6
Gabriel Cavalcante <gabrielcavalcante1021@gmail.com>
2019-05-12 16:22:11 -0300

    PEP8 code reformating

--- simple_blackjack_python/blackjack_code/blackjack.py
+++ simple_blackjack_python/blackjack_code/blackjack.py
@@ -1,11 +1,16 @@
-# A simple blackjack game contains all 52 cards in a deck, with 4 sets of 13 cards.
+"""
+Simple Black Jack in Python 3
+A simple blackjack game contains all 52 cards in a deck, with 4 sets of 13 cards.
+"""
+
 import random
 import os
+
 
 # Function to calculate the hand.
 # In order to calculate the hand you have to pass the hand.
 def calc_hand(hand):
-    sum = 0
+    sum_value = 0
 
     # Using lists comprehension.
     # Allows creation of a list which iterates over an existing list.
@@ -15,35 +20,34 @@ def calc_hand(hand):
 
     for card in non_aces:
         if card in 'JQK':
-            sum += 10
+            sum_value += 10
         else:
-            sum += int(card)
+            sum_value += int(card)
 
     for card in aces:
-        if sum <= 10:
-            sum += 11
+        if sum_value <= 10:
+            sum_value += 11
         else:
-            sum += 1
+            sum_value += 1
 
-    return sum
+    return sum_value
+
 
 # Cards of the deck.
 cards = [
-    '2','3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A',
-    '2','3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A',
-    '2','3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A',
-    '2','3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A',
+    '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A',
+    '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A',
+    '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A',
+    '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A',
 ]
 
 random.shuffle(cards)
 
 dealer = []
-player = []
+player = [cards.pop(), cards.pop()]
 
 # Adds a card to the player, then adds one to the dealer
-player.append(cards.pop())
 dealer.append(cards.pop())
-player.append(cards.pop())
 dealer.append(cards.pop())
 
 standing = False
@@ -58,7 +62,8 @@ while True:
     dealer_score = calc_hand(dealer)
 
     if standing:
-        print('Dealer Cards: [{}] ({})'.format(']['.join(dealer), dealer_score))
+        print('Dealer Cards: [{}] ({})'.format(
+            ']['.join(dealer), dealer_score))
     else:
         print('Dealer Cards: [{}][?]'.format(dealer[0]))
 
@@ -94,4 +99,3 @@ while True:
         standing = True
         while calc_hand(dealer) <= 16:
             dealer.append(cards.pop())
-
